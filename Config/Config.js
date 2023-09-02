ca56de89fddd86e84e29a4e48a46b1cf74363b1f
Kimo <mk5391130@gmail.com>
2023-09-03 01:59:36 +0300

    f

--- Config/Config.js
+++ Config/Config.js
@@ -1,25 +1,27 @@
 import pkg from 'dotenv';
 const {dotenv} = pkg;
-export default  {
-    PORT_NO : process.env.PORT_NO,
 
-    DB_URL : process.env.DB_URL,
-    
-    DB_NAME : process.env.DB_NAME,
-    
-    HASH_KEY : process.env.HASH_KEY,
-    
-    EXP_PER : process.env.EXP_PER,
-    
-    SECRET_KEY : process.env.SECRET_KEY,
-    
-    MAIL_USERNAME :  process.env.MAIL_USERNAME,
-    
-    MAIL_PASSWORD : process.env.MAIL_PASSWORD,
-    
-    TIME_STAMP :  process.env.TIME_STAMP,
+let obj = {
+    PORT_NO :process.env.PORT_NO,
 
-    DB_USERNAME : process.env.DB_USERNAME,
-    DB_PASSWORD : process.env.DB_PASSWORD
-    
+   DB_URL : process.env.DB_URL,
+   
+   DB_NAME : process.env.DB_NAME,
+   
+   HASH_KEY : process.env.HASH_KEY,
+   
+   EXP_PER : process.env.EXP_PER,
+   
+   SECRET_KEY : process.env.SECRET_KEY,
+   
+   MAIL_USERNAME :  process.env.MAIL_USERNAME,
+   
+   MAIL_PASSWORD : process.env.MAIL_PASSWORD,
+   
+   TIME_STAMP :  process.env.TIME_STAMP,
+
+   DB_USERNAME : process.env.DB_USERNAME,
+   DB_PASSWORD : process.env.DB_PASSWORD
+   
 }
+export default obj 
