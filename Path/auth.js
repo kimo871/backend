8e240d5acccd3ab1fe05a6887f2425cf8d267ffa
Kimo <mk5391130@gmail.com>
2023-09-04 02:09:01 +0300

    j

--- Path/auth.js
+++ Path/auth.js
@@ -24,7 +24,7 @@ Router.post("/refresh-token",async (req,res)=>{
         const token = jwt.sign({Email:Email},SECRET_KEY,{expiresIn:EXP_PER})
         res.status(200)
         res.header("Access-Control-Allow-Origin","https://codsoft-1-z2b7.onrender.com");
-        res.cookie("access-token",token,{path:"/",secure:true,domain:".onrender.com"})
+        res.cookie("access-token",token,{path:"/",secure:true,domain:"codsoft-1-z2b7.onrender.com"})
         res.send()
     }
     else{
@@ -65,7 +65,7 @@ Router.get("/login",Authentication,(req,res)=>{
         res.header("Access-Control-Allow-Origin","https://codsoft-1-z2b7.onrender.com");
         res.header('Access-Control-Allow-Credentials', true);
         res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.cookie("access_token",req.token,{path:"/",httpOnly:false,sameSite:"none",secure:true,domain:".onrender.com"})
+    res.cookie("access_token",req.token,{path:"/",httpOnly:false,sameSite:"none",secure:true,domain:"codsoft-1-z2b7.onrender.com"})
     res.json({"user":obj});
 }).catch(err=> res.status(400).send())
 
